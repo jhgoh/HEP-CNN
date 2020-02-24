f758ba12a164ce692bfe99fa203f403735e6d33d
Junghwan John Goh <jhgoh@cern.ch>
2020-02-24 11:28:38 +0900

    try deeper model

--- python/HEPCNN/torch_model_gcn.py
+++ python/HEPCNN/torch_model_gcn.py
@@ -29,10 +29,11 @@ class MyModel(nn.Module):
         super(MyModel, self).__init__()
         self.nChannel = 3
 
-        self.conv1 = PointDynamicEdgeConv(MLP([self.nChannel+3, 64, 64, 64]), 8, 'max')
-        self.conv2 = PyG.DynamicEdgeConv(MLP([2 * 64, 128]), 8, 'max')
+        self.conv1 = PointDynamicEdgeConv(MLP([self.nChannel+3, 64, 128]), 8, 'max')
+        self.conv2 = PyG.DynamicEdgeConv(MLP([2 * 128, 256]), 4, 'max')
+        self.conv3 = PyG.DynamicEdgeConv(MLP([2 * 256, 512]), 4, 'max')
 
-        self.lin1 = MLP([128 + 64, 1024])
+        self.lin1 = MLP([512 + 256 + 128, 1024])
 
         self.fc = nn.Sequential(
             nn.Linear(1024, 512), nn.ReLU(), nn.BatchNorm1d(512), nn.Dropout(0.5),
@@ -45,7 +46,8 @@ class MyModel(nn.Module):
         x, pos, batch = data.x, data.pos, data.batch
         x1 = self.conv1(x, pos, batch)
         x2 = self.conv2(x1, batch)
-        out = self.lin1(torch.cat([x1, x2], dim=1))
+        x3 = self.conv3(x2, batch)
+        out = self.lin1(torch.cat([x1, x2, x3], dim=1))
         out = PyG.global_max_pool(out, batch)
         out = self.fc(out)
         return out
