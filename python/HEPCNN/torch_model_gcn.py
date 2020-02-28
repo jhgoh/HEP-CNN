3aea02867a607106dd70b6d23b4b44ec01d8a4b2
Junghwan John Goh <jhgoh@cern.ch>
2020-02-29 03:19:19 +0900

    clustering with radius, try shallow layers

--- python/HEPCNN/torch_model_gcn.py
+++ python/HEPCNN/torch_model_gcn.py
@@ -12,13 +12,14 @@ def MLP(channels, batch_norm=True):
     ])
 
 class PointConvNet(nn.Module):
-    def __init__(self, net, k, **kwargs):
+    def __init__(self, net, r, **kwargs):
         super(PointConvNet, self).__init__()
-        self.k = k
+        self.r = r
         self.conv = PyG.PointConv(net)
 
     def forward(self, x, pos, batch=None):
-        edge_index = PyG.knn_graph(pos, self.k, batch, loop=False, flow='source_to_target')
+        #edge_index = PyG.knn_graph(pos, self.k, batch, loop=False, flow='source_to_target')
+        edge_index = PyG.radius_graph(pos, self.r, batch, loop=False)
         x = self.conv(x, pos, edge_index)
         return x, pos, batch
 
@@ -39,13 +40,13 @@ class MyModel(nn.Module):
         super(MyModel, self).__init__()
         self.nChannel = 3
 
-        self.conv1 = PointConvNet(MLP([self.nChannel+3, 64, 64, 128]), 8)
-        self.conv2 = PointConvNet(MLP([3+128, 128, 128, 256]), 4)
+        self.conv1 = PointConvNet(MLP([self.nChannel+3, 64, 64, 256]), 0.2)
+        #self.conv2 = PointConvNet(MLP([3+128, 128, 128, 256]), 0.2)
         self.pool = PoolingNet(MLP([256 + 3, 256, 512, 1024]))
 
         self.fc = nn.Sequential(
-            nn.Linear(1024, 512), nn.ReLU(), nn.BatchNorm1d(512), nn.Dropout(0.5),
-            nn.Linear( 512, 256), nn.ReLU(), nn.BatchNorm1d(256), nn.Dropout(0.5),
+            nn.Linear(1024, 256), nn.ReLU(), nn.BatchNorm1d(256), nn.Dropout(0.5),
+            #nn.Linear( 512, 256), nn.ReLU(), nn.BatchNorm1d(256), nn.Dropout(0.5),
             nn.Linear( 256,   1),
             nn.Sigmoid(),
         )
@@ -53,7 +54,7 @@ class MyModel(nn.Module):
     def forward(self, data):
         x, pos, batch = data.x, data.pos, data.batch
         x, pos, batch = self.conv1(x, pos, batch)
-        x, pos, batch = self.conv2(x, pos, batch)
+        #x, pos, batch = self.conv2(x, pos, batch)
         x, pos, batch = self.pool(x, pos, batch)
         out = self.fc(x)
         return out
