e9dcd02b11412a40cc3514023da64a0d80f8a383
Junghwan John Goh <jhgoh@cern.ch>
2020-03-06 02:12:02 +0900

    better plotting style

--- run/drawLossCurve.py
+++ run/drawLossCurve.py
@@ -14,6 +14,14 @@ ax2 = plt.subplot(2, 3, 2, yscale='log', ylabel='Loss(val)', xlabel='epoch')
 #ax2 = plt.subplot(2, 3, 2, ylabel='Loss(val)')
 ax3 = plt.subplot(2, 3, 4, ylabel='Accuracy(train)', xlabel='epoch')
 ax4 = plt.subplot(2, 3, 5, ylabel='Accuracy(val)', xlabel='epoch')
+ax1.set_ylim([3e-2,1])
+ax2.set_ylim([3e-2,1])
+ax3.set_ylim([0.85,1])
+ax4.set_ylim([0.85,1])
+for ax in (ax1, ax2, ax3, ax4):
+    ax.grid(which='major', axis='both', linestyle='-.')
+    ax.grid(which='minor', linestyle=':')
+    ax.set_xlim([0,50])
 lines, labels = [], []
 for d in sys.argv[1:]:
     df = pd.read_csv(d)
@@ -25,7 +33,6 @@ for d in sys.argv[1:]:
 
     ax3.plot(df['acc'], '.-', label=label)
     ax4.plot(df['val_acc'], '.-', label=label)
-    #ax.set_ylim([0,1])
 
     lines.append(l[0])
     labels.append(label)
