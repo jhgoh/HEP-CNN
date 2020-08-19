26b4d370d5ee989f01894db9b2eaa3aae9ae80b9
Junghwan John Goh <jhgoh@cern.ch>
2020-08-20 01:06:39 +0900

    read from gzipped csv

--- run/drawLossCurve.py
+++ run/drawLossCurve.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys, os
+import gzip
 
 plt.rcParams['lines.linewidth'] = 1
 plt.rcParams['lines.markersize'] = 5
@@ -21,12 +22,13 @@ ax4 = plt.subplot(3, 2, 4, ylabel='Accuracy(val)', xlabel='epoch')
 for ax in (ax1, ax2, ax3, ax4):
     ax.grid(which='major', axis='both', linestyle='-.')
     ax.grid(which='minor', linestyle=':')
-    ax.set_xlim([0,50])
+    #ax.set_xlim([0,50])
 lines, labels = [], []
 for d in sys.argv[1:]:
-    df = pd.read_csv(d)
-
     label = d.split('/')[-2].replace('__', ' ').replace('_', '=')
+
+    if d.endswith('gz'): d = gzip.open(d)
+    df = pd.read_csv(d)
 
     l = ax1.plot(df['loss'], '.-', label=label)
     ax2.plot(df['val_loss'], '.-', label=label)
